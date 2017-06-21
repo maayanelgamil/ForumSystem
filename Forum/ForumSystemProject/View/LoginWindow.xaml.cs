7ca609bf75d5af7822cf326bb3538b03b1034e3d
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-06-21 18:07:32 +0300

    all

---
+++ Forum/ForumSystemProject/View/LoginWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ForumSystemProject.Controller;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ForumSystemProject.View
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        IController controller;
+
+        /// <summary>
+        /// C'tor for the LoginWindow class
+        /// </summary>
+        /// <param name="_controller"></param>
+        public LoginWindow(IController _controller)
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+            controller = _controller;
+        }
+
+        /// <summary>
+        /// This function defines the actions taken after the user pressed the 'connect' button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void b_connect_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = controller.connect(tb_mail.Text, tb_password.Text);
+            if (dt == null)
+                MessageBox.Show("שם המשתמש ו/או הסיסמא אינם תקינים, נסה שנית או הרשם למערכת");
+            else
+            {
+                UserWindow userWin = new UserWindow(tb_mail.Text, controller);
+                Visibility = Visibility.Hidden;
+                userWin.Closed += UserWin_Closed;
+                userWin.Show();
+            }
+        }
+
+        private void UserWin_Closed(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Forum/ForumSystemProject/View/UserWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using ForumSystemProject.Controller;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ForumSystemProject.View
+{
+    /// <summary>
+    /// Interaction logic for UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        IController controller;
+        string mail;
+
+        public UserWindow(string mail, IController _controller)
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+            controller = _controller;
+            this.mail = mail;
+
+            DataTable dt = controller.getFirstNameData(mail);
+            DataRow[] rows = dt.Select();
+            txt_firstName.Text = "," + rows[0]["firstName"].ToString();
+        }
+
+
+        private void search_button_Click(object sender, RoutedEventArgs e)
+        {
+            Search_window searchWin = new Search_window(controller);
+            Visibility = Visibility.Hidden;
+            searchWin.Closed += SearchWin_Closed;
+            searchWin.Show();
+        }
+
+        private void SearchWin_Closed(object sender, EventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("אופציה זו אינה נתמכת בגירסה הנוכחית");
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("אופציה זו אינה נתמכת בגירסה הנוכחית");
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            //CreateProfileByInterestArea_window addProfileWindow = new CreateProfileByInterestArea_window(controller, mail);
+            //addProfileWindow.Closed += addProfileWindow_Closed; //close this window when add profile is closed
+            ///this.Visibility = Visibility.Hidden; //hide this window until add profile ended
+            //addProfileWindow.Show();
+        }
+
+        void addProfileWindow_Closed(object sender, EventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+        }
+
+
+    }
+}
